aa6681dfdff83062017b3d9bf37593e1f00f1044
alokprasad573 <alokprasad573@outlook.com>
2025-06-05 03:01:33 +0530

    Heading Added.

--- src/components/Instruction.jsx
+++ src/components/Instruction.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 export default function Instruction() {
     return (
         <div className="text-center text-white my-8 text-5xl">
+            <h1 className='underline'>Instructions</h1>
+            <br />
             <p>Player 1 : <span className="text-[rgb(160,21,62)]">X</span></p>
             <br />
             <p>Player 2 : <span className="text-[rgb(51,115,87)]">0</span></p>
